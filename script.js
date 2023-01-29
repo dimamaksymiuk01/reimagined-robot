1b16af53b583e1c79da40f3a35cc99051b1aed61
dimamaksymiuk01 <dimamaksymiuk01@gmail.com>
2023-01-29 17:14:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+console.log("Home work");
+
+// // tusk 1
+// let question = prompt("What's your name? ");
+// console.log(question);
+// alert(`Hi ${question}`);
+
+// // tusk 2
+// const old = 2023;
+// let year = +prompt("In what year were you born?: ");
+// let result = old - year;
+// alert (`You already ${result} years old `);
+
+// // tusk 3
+// let length = +prompt("What is the length of the side of the square?: ");
+// let result = length * 4;
+// alert(`Perimeter of a square = ${result}`);
+
+// // tusk 4
+// let square = +prompt("Enter the radius of the circle: ");
+// let result = square * square * 3.14;
+// alert(`Circle area = ${result}`);
+
+// // tusk 5
+// const mile = 0.621371;
+// let km = +prompt("Enter the number of kilometers to your city: ");
+// let hours = +prompt("How many hours do you want to get there?:");
+// let result = km / hours;
+// alert(`You need to go ${result} km/h`);
+
+// // tusk 6 
+// const euro = 1.09;
+// let dollar = +prompt("How many dollars do you have? ");
+// let exchange = dollar / euro;
+// alert(`You will have ${exchange} euro`);
+
+// // tusk 7
+// let gb = +prompt ("How much GB is your flash drive?: ");
+// let result = (gb * 1024) / 820;
+// alert (`You can download: ${result} files`);
+
+// // tusk 8 
+// let wallet = +prompt("How much money do you have? ");
+// let price = +prompt("What is the cost of 1 chocolate bar? ");
+// let result = Math.floor(wallet/price);
+// let result2 =  wallet % price;
+// alert(`You will have ${result} chocolate and ${result2} the rest of the money`);
+
+// // tusk 9
+// let a = +prompt("Enter a three-digit number");
+// let number3 = a % 10;
+// a = Math.floor(a / 10);
+// let number2 = a % 10;
+// a = Math.floor(a / 10);
+// let number1 = a % 10;
+// let result = "" + number3 + number2 + number1;
+// alert(`You get a palindrome ${result}`);
+
+// // tusk 10
+// let a = +prompt ("Enter number");
+// let result = (a % 2) === 1 ? "Not an even number" : "Even number"
+// alert(`${result}`);
